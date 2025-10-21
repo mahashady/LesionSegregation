d7050ac9b8343e54ef8cec4138a48352982b9685
andrianovama <maria.andrianova@irbbarcelona.org>
2025-10-21 10:35:05 +0200

    Update folder structure

---
+++ data_analysis/mice/LAD/scripts/HMM_ploidy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=run_hmm_subclone     # Job name
+#SBATCH --time=02:00:00
+#SBATCH -c 8
+#SBATCH --mem 16gb
+#SBATCH --array=0-21%20   # Job array range, limit to 5 simultaneous jobs
+
+
+source activate cancer_architecture
+# Path to the directory containing .hmm files
+input_dir="/results/HMM/input_HMM"
+# Get the list of .hmm files into an array
+files=("$input_dir"/*.hmm)
+
+# Get the file corresponding to the current job index ($SLURM_ARRAY_TASK_ID)
+file=${files[$SLURM_ARRAY_TASK_ID]}
+
+# Extract filename without path and extension
+filename=$(basename "$file")
+
+echo "Processing $filename"
+
+# Run the R script and pass the filename as an argument
+Rscript HMM_subclone.R "$filename"
--- data_analysis/mice/error_free/fit_2betas_1div_4cells_only.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --time=00:30:00
-#SBATCH -c 8
-#SBATCH --mem 16gb
-
-
-source activate cancer_architecture
-
-Rscript --vanilla fit_2betas_1div_4cells_only.R 
